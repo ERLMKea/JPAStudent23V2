ac562b6bd4bfacd9f9a3b757a6575e1c90bf20ad
erlm <erlm@kea.dk>
2024-02-19 19:09:29 +0100

    set logging level at runtime

--- src/main/java/com/example/jpastudent23v2/controller/StudentRestController.java
+++ src/main/java/com/example/jpastudent23v2/controller/StudentRestController.java
@@ -4,13 +4,14 @@ import com.example.jpastudent23v2.JpaStudent23V2Application;
 import com.example.jpastudent23v2.model.Student;
 import com.example.jpastudent23v2.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentRestController {
@@ -48,4 +49,53 @@ public class StudentRestController {
         return studentRepository.findAllByName(name);
     }
 
+    @PostMapping("/student")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student postStudent(@RequestBody Student student) {
+        System.out.println(student);
+        studentRepository.save(student);
+        return student;
+    }
+
+    @PutMapping("/studentx")
+    public Student putStudentx(@RequestBody Student student) {
+        //return studentRepository.save(student);
+        Optional<Student> student1 = studentRepository.findById(student.getId());
+        if (student1.isPresent()) {
+            return studentRepository.save(student);
+        } else {
+            Student notStudent = new Student();
+            notStudent.setName("NOT FOUND");
+            return notStudent;
+        }
+    }
+
+    @PutMapping("/student")
+    public ResponseEntity<Student> putStudent(@RequestBody Student student) {
+        //return studentRepository.save(student);
+        Optional<Student> student1 = studentRepository.findById(student.getId());
+        if (student1.isPresent()) {
+            studentRepository.save(student);
+            return new ResponseEntity<>(student, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(new Student(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/student/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable int id) {
+        Optional<Student> student1 = studentRepository.findById(id);
+        if (student1.isPresent()) {
+            studentRepository.deleteById(id);
+            return ResponseEntity.ok("Student delete");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found id=" + id);
+        }
+    }
+
 }
+
+
+
+
+
